058d0d9a498d9881f6e4dee59c15637d576b31a2
judeallanhill <judeallanhill@icloud.com>
2023-07-02 20:11:14 -0700

    Adding Player.js

--- Game.js
+++ Game.js
@@ -1,13 +1,14 @@
 import {Rect} from "./RectUtils.js"
 import { ParticleSource } from "./Particals.js";
 import { Mouse,Button } from "./MouseEngine.js";
+import { Player } from "./Player.js";
 let canvas = document.getElementById("canvas")
 let ctx = canvas.getContext("2d")
-let currentKey = new Map();
-let multiplyer = 1;
-let powerUpMultiplyer = 300;
-let RandomNumDeathBrick = 0; 
-let NumToMatchDeathBrick = 0;
+export let currentKey = new Map();
+export let multiplyer = 1;
+export let powerUpMultiplyer = 300;
+export let RandomNumDeathBrick = 0; 
+export let NumToMatchDeathBrick = 0;
 let PowerupRandomNum = Math.floor(Math.random()*powerUpMultiplyer+1)
 let NumToMatchPowerUp = Math.floor(Math.random()*powerUpMultiplyer+1)
 let mode = "menu";
@@ -140,93 +141,6 @@ class Boss {
         this.bounds.y = 200
         this.intersected = false
         this.direction = 1
-    }
-}
-class Player {
-    constructor() {
-        this.bounds = new Rect(200,300,18,18);
-        this.speed = 2;
-        this.size = 50;
-        this.helth = 100
-        this.alive = true;
-        this.Frags = 0;
-        this.LevelFrags = 0;
-        this.invicable = false;
-        this.dash = true;
-        this.tempSpeed = 2;
-        this.die = new Audio();
-        this.GameWidth = 18;
-        this.GameHeight = 18;
-        this.die.src = "./Assets/explosion.wav"
-    }
-    draw() {
-        ctx.fillStyle = "#5a473e"
-        ctx.fillRect(this.bounds.x,this.bounds.y,this.bounds.w,this.bounds.h)
-        ctx.fillStyle = "green"
-        ctx.fillRect(20,10+5,this.helth*2,40)
-        ctx.strokeStyle = "black"
-        ctx.lineWidth = 5
-        ctx.strokeRect(20,15,200,40)
-    }
-    
-    init() {
-        this.bounds.w = this.GameWidth;
-        this.bounds.h = this.GameHeight;
-        this.tempSpeed = this.speed;
-        RandomNumDeathBrick = Math.floor(Math.random() * multiplyer+1); 
-        NumToMatchDeathBrick = Math.floor(Math.random()* multiplyer+1)
-        PowerupRandomNum = Math.floor(Math.random()*powerUpMultiplyer+1);
-        NumToMatchPowerUp = Math.floor(Math.random()*powerUpMultiplyer+1)
-    } 
-    update() {
-        this.bounds.w += 0.02;
-        this.bounds.h += 0.02;
-
-        if (currentKey.get("w") ) {
-            this.bounds.y -= this.tempSpeed;
-        }
-        if (currentKey.get("s")) {
-            this.bounds.y += this.tempSpeed;
-        }
-        if (currentKey.get("a")) {
-            this.bounds.x -= this.tempSpeed;
-        }
-        if (currentKey.get("d")) {
-            this.bounds.x += this.tempSpeed;
-        }
-        for (let i = 0; i < deathBricks.length; i++) {
-            if (deathBricks[i].bounds.intersects(this.bounds) || this.bounds.intersects(deathBricks[i].bounds)) {
-                    this.die.play();
-                    this.alive = false;
-                    currentKey.clear();
-            }
-        }
-        if (this.alive === false) {
-            alert("You Died!")
-            mode = "menu";
-        }
-        if ( this.bounds.y < 0 ) {
-            this.bounds.y = 0.5;
-        }
-        if ( this.bounds.x < 0 ) {
-            this.bounds.x = 0.5;
-        }
-        if ( this.bounds.y > canvas.height-20 ) {
-            this.bounds.y = canvas.height-20;
-        }
-        if ( this.bounds.x > canvas.width-this.bounds.w) {
-            this.bounds.x = canvas.width-this.bounds.w;
-        }
-    }
-    reset() {
-        this.bounds.x = 200;
-        this.bounds.y = 300;
-        this.bounds.w = this.GameWidth;
-        this.bounds.h = this.GameHeight;
-        this.tempSpeed = this.speed;
-        this.size = 18;
-        this.helth = 100;
-        this.alive = true;
     }
 }
 class GoldKey {
@@ -363,7 +277,7 @@ function JSON() {
     console.error('Error:', error);
   });
 }
-let deathBricks = []
+export let deathBricks = []
 let powerups = []
 let player = new Player();
 let goldKey = new GoldKey();
@@ -372,6 +286,12 @@ let particalEngine = new ParticleSource();
 let mouse = new Mouse();
 let SpeedUpgradeButton = new Button();
 let BackButton = new Button();
+function GameInit() {
+    RandomNumDeathBrick = Math.floor(Math.random() * multiplyer+1); 
+    NumToMatchDeathBrick = Math.floor(Math.random()* multiplyer+1)
+    PowerupRandomNum = Math.floor(Math.random()*powerUpMultiplyer+1);
+    NumToMatchPowerUp = Math.floor(Math.random()*powerUpMultiplyer+1)
+}
 function keyboardInit() {
     window.addEventListener("keydown", function (event) {
         currentKey.set(event.key, true);
@@ -390,6 +310,7 @@ function loop() {
         document.getElementById("time").style.visibility = "hidden"
         FragsELEMENT.style.visibility = "visible"
         player.init();
+        GameInit();
 
     }
     if (mode != "menu") {
---
+++ Player.js
@@ -0,0 +1,84 @@
+import {currentKey, deathBricks} from "./Game.js";
+import { Rect } from "./RectUtils.js";
+export class Player {
+    constructor() {
+        this.bounds = new Rect(200,300,18,18);
+        this.speed = 2;
+        this.size = 50;
+        this.helth = 100
+        this.alive = true;
+        this.Frags = 0;
+        this.LevelFrags = 0;
+        this.invicable = false;
+        this.dash = true;
+        this.tempSpeed = 2;
+        this.die = new Audio();
+        this.GameWidth = 18;
+        this.GameHeight = 18;
+        this.die.src = "./Assets/explosion.wav"
+    }
+    draw(ctx) {
+        ctx.fillStyle = "#5a473e"
+        ctx.fillRect(this.bounds.x,this.bounds.y,this.bounds.w,this.bounds.h)
+        ctx.fillStyle = "green"
+        ctx.fillRect(20,10+5,this.helth*2,40)
+        ctx.strokeStyle = "black"
+        ctx.lineWidth = 5
+        ctx.strokeRect(20,15,200,40)
+    }
+    init() {
+        this.bounds.w = this.GameWidth;
+        this.bounds.h = this.GameHeight;
+        this.tempSpeed = this.speed;
+    } 
+    update() {
+        this.bounds.w += 0.02;
+        this.bounds.h += 0.02;
+
+        if (currentKey.get("w") ) {
+            this.bounds.y -= this.tempSpeed;
+        }
+        if (currentKey.get("s")) {
+            this.bounds.y += this.tempSpeed;
+        }
+        if (currentKey.get("a")) {
+            this.bounds.x -= this.tempSpeed;
+        }
+        if (currentKey.get("d")) {
+            this.bounds.x += this.tempSpeed;
+        }
+        for (let i = 0; i < deathBricks.length; i++) {
+            if (deathBricks[i].bounds.intersects(this.bounds) || this.bounds.intersects(deathBricks[i].bounds)) {
+                    this.die.play();
+                    this.alive = false;
+                    currentKey.clear();
+            }
+        }
+        if (this.alive === false) {
+            alert("You Died!")
+            mode = "menu";
+        }
+        if ( this.bounds.y < 0 ) {
+            this.bounds.y = 0.5;
+        }
+        if ( this.bounds.x < 0 ) {
+            this.bounds.x = 0.5;
+        }
+        if ( this.bounds.y > canvas.height-20 ) {
+            this.bounds.y = canvas.height-20;
+        }
+        if ( this.bounds.x > canvas.width-this.bounds.w) {
+            this.bounds.x = canvas.width-this.bounds.w;
+        }
+    }
+    reset() {
+        this.bounds.x = 200;
+        this.bounds.y = 300;
+        this.bounds.w = this.GameWidth;
+        this.bounds.h = this.GameHeight;
+        this.tempSpeed = this.speed;
+        this.size = 18;
+        this.helth = 100;
+        this.alive = true;
+    }
+}
